813489608338aa222cae1b7ca380c919e678db7d
Ionut V <67919936+ionutMan07@users.noreply.github.com>
2020-09-01 12:31:43 +0300

    Initial commit

---
+++ arrays.js
@@ -0,0 +1,33 @@
+var myArray = [1, 2, 3, 4, 5];
+console.log(myArray);
+
+myArray.push(10);
+
+console.log("index of 1: ", myArray.indexOf(1));
+
+console.log(myArray.length);
+
+console.log("join", myArray.join());
+
+// suma elementelor unui array
+
+var sum = 0;
+for (var i = 0; i < myArray.length; i++) {
+  // console.log("i:", i);
+  // console.log("sum:", sum);
+  // console.log("current element: ", myArray[i]);
+  // console.log("_____________________");
+  sum += myArray[i];
+}
+console.log("Sum of array is: ", sum);
+
+// elementul maxim
+
+var max = myArray[0];
+for (var i = 0; i < myArray.length; i++) {
+  var currentItem = myArray[i];
+  if (currentItem > max) {
+    max = currentItem;
+  }
+}
+console.log("Largest number is: ", max);
---
+++ object.js
@@ -0,0 +1,64 @@
+const Car1 = {
+  brand: "Dacia",
+  model: "Logan",
+  year: 2010,
+  color: "Red"
+};
+
+const Car2 = {
+  brand: "Ford",
+  model: "Focus",
+  year: 2018,
+  color: "White"
+};
+
+const Car3 = {
+  brand: "Honda",
+  model: "Civic",
+  year: 2009,
+  color: "White"
+};
+
+var carList = [Car1, Car2];
+
+carList.push(Car3);
+console.log(Car1.year);
+console.log(Car2.brand);
+
+Car1.year = 2012;
+
+console.log(Car1.year);
+
+Car1.mileage = 130000;
+
+var myPropertyName = "price";
+
+Car1[myPropertyName] = 5000;
+
+Car1.myPropertyName = "something";
+
+delete Car1.myPropertyName;
+
+console.log(Car1);
+
+console.log(Car1.year < Car2.year);
+
+var keys = Object.keys(Car1);
+
+console.log(keys);
+
+// oldest model year
+
+var result = carList[0].year;
+
+for (var i = 0; i < carList.length; i++) {
+  var car = carList[i];
+  if (car.year < result) {
+    result = car.year;
+  }
+  if (car.model === "Civic") {
+    console.log("🚗");
+  }
+}
+
+console.log("Oldest year is: ", result);
