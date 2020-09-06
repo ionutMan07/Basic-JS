6c5a6df546db097ae0d5d06c544dffb2e99a4d4f
Ionut V <67919936+ionutMan07@users.noreply.github.com>
2020-09-06 11:42:14 +0300

    new

--- arrays.js
+++ arrays.js
@@ -13,17 +13,17 @@ console.log(myArray);
 
 var sum = 0;
 for (var i = 0; i < myArray.length; i++) {
-  console.log("i:", i);
-  console.log("current element: ", myArray[i]);
-  console.log("sum:", sum);
-  console.log("_____________________");
+  // console.log("i:", i);
+  // console.log("current element: ", myArray[i]);
+  // console.log("sum:", sum);
+  // console.log("_____________________");
   sum += myArray[i];
-  console.log("sum:", sum);
-  console.log("_____________________");
+  // console.log("sum:", sum);
+  // console.log("_____________________");
 }
 console.log("Sum of array is: ", sum);
 
-// // elementul maxim
+// elementul maxim
 
 // var max = myArray[0];
 // for (var i = 0; i < myArray.length; i++) {
@@ -32,4 +32,12 @@ console.log("Sum of array is: ", sum);
 //     max = currentItem;
 //   }
 // }
-// console.log("Largest number is: ", max);
+//  console.log("Largest number is: ", max);
+
+let max = 0;
+for (let i of myArray) {
+  if (i > max) {
+    max = i;
+  }
+}
+console.log("Max number of: ", myArray, "is", max);
